b26bc98896a7f12a146a006fb3506d811a66f97f
Dominance & Delusion <gjnicholsiii@gmail.com>
2026-08-17 15:29:04 -0500

    Add Pennsylvania to reusable Jaggaer connector

--- lib/sled/jaggaer.ts
+++ lib/sled/jaggaer.ts
@@ -14,6 +14,7 @@ const JAGGAER_STATES: JaggaerStateConfig[] = [
   { stateCode: "IA", stateName: "Iowa", customerOrg: "DASIowa", sourceName: "Iowa IMPACS", agencyName: "State of Iowa" },
   { stateCode: "MT", stateName: "Montana", customerOrg: "StateOfMontana", sourceName: "Montana eMACS", agencyName: "State of Montana" },
   { stateCode: "UT", stateName: "Utah", customerOrg: "StateOfUtah", sourceName: "Utah Public Procurement Place", agencyName: "State of Utah" },
+  { stateCode: "PA", stateName: "Pennsylvania", customerOrg: "CommonwealthPA", sourceName: "Pennsylvania JAGGAER Public Events", agencyName: "Commonwealth of Pennsylvania" },
 ];
 
 export interface JaggaerProbeResult {
